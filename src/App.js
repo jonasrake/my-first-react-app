e2ef095f7501b0eb0a5c376ff7e8319a43724996
jonasrake <jonasrake@gmail.com>
2021-09-27 11:05:15 +0200

    Fixed errors in app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p><h1>This is a line of HTML with an "h1" tag on my first react application</h1></p>
-      <p><h2>This is a line of HTML with an "h2" tag on my first react application</h2></p>
-      <p><h3>This is a line of HTML with an "h3" tag on my first react application</h3></p>
-      <p><h4>This is a line of HTML with an "h4" tag on my first react application</h4></p>
+      <h1>This is a line of HTML with an "h1" tag on my first react application</h1>
+      <h2>This is a line of HTML with an "h2" tag on my first react application</h2>
+      <h3>This is a line of HTML with an "h3" tag on my first react application</h3>
+      <h4>This is a line of HTML with an "h4" tag on my first react application</h4>
       <p>This is a line of HTML with no header tag on my first react application</p>
       <Button/>
+      <button>click me</button>
     </div>
   );
 }
